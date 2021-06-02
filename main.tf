1d7ccae775b93c169700650731248e30a646d819
Han Yan <han.yan@nordstrom.com>
2021-06-02 17:05:37 -0600

    Added role_arn

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region = var.aws_region
-#   assume_role {
-#     role_arn = "arn:aws:sts::182162384657:assumed-role/NonProd_MLDataPipelines_DevUsers"
-#   }
+  assume_role {
+    role_arn = "arn:aws:sts::182162384657:assumed-role/NonProd_MLDataPipelines_DevUsers"
+  }
 }
 
 
